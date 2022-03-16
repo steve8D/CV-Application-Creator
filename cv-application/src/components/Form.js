d96f5a923d1207ca68248f93dc931043bf01e147
Danny Ngo <70412140+steve8D@users.noreply.github.com>
2022-03-15 21:19:04 -0700

    Added boxing to CV Form

--- cv-application/src/components/Form.js
+++ cv-application/src/components/Form.js
@@ -13,76 +13,79 @@ const Form = ({
   handleExperienceRemove,
 }) => {
   return (
-    <div className="Form">
+    <div className="form">
       <h2>Basic Information</h2>
-      <Input
-        id="firstName"
-        name="firstName"
-        value={info.firstName}
-        onInputChange={onInputChange}
-      >
-        First Name
-      </Input>
-      <br />
-      <Input
-        id="lastName"
-        name="lastName"
-        value={info.lastName}
-        onInputChange={onInputChange}
-      >
-        Last Name
-      </Input>
-      <br />
-      <Input
-        id="email"
-        name="email"
-        value={info.email}
-        onInputChange={onInputChange}
-      >
-        Email
-      </Input>
-      <br />
-      <Input
-        id="phoneNo"
-        name="phoneNo"
-        value={info.phoneNo}
-        onInputChange={onInputChange}
-      >
-        Phone Number
-      </Input>
-      <br />
+      <Box className="personal-info">
+        <Input
+          id="firstName"
+          name="firstName"
+          value={info.firstName}
+          onInputChange={onInputChange}
+        >
+          First Name
+        </Input>
+        <br />
+        <Input
+          id="lastName"
+          name="lastName"
+          value={info.lastName}
+          onInputChange={onInputChange}
+        >
+          Last Name
+        </Input>
+        <br />
+        <Input
+          id="email"
+          name="email"
+          value={info.email}
+          onInputChange={onInputChange}
+        >
+          Email
+        </Input>
+        <br />
+        <Input
+          id="phoneNo"
+          name="phoneNo"
+          value={info.phoneNo}
+          onInputChange={onInputChange}
+        >
+          Phone Number
+        </Input>
+      </Box>
 
       <h2>Education</h2>
-      <Input
-        id="university"
-        name="university"
-        value={info.university}
-        onInputChange={onInputChange}
-      >
-        School
-      </Input>
-      <br />
-      <Input
-        id="degreeTitle"
-        name="degreeTitle"
-        value={info.degreeTitle}
-        onInputChange={onInputChange}
-      >
-        Degree and Major
-      </Input>
-      <br />
-      <Input
-        id="graduationDate"
-        name="graduationDate"
-        value={info.graduationDate}
-        onInputChange={onInputChange}
-      >
-        Graduation Date
-      </Input>
+      <Box className="education-info">
+        <Input
+          id="university"
+          name="university"
+          value={info.university}
+          onInputChange={onInputChange}
+        >
+          School
+        </Input>
+        <br />
+        <Input
+          id="degreeTitle"
+          name="degreeTitle"
+          value={info.degreeTitle}
+          onInputChange={onInputChange}
+        >
+          Degree and Major
+        </Input>
+        <br />
+        <Input
+          id="graduationDate"
+          name="graduationDate"
+          value={info.graduationDate}
+          onInputChange={onInputChange}
+        >
+          Graduation Date
+        </Input>
+      </Box>
 
       <h2>Experiences</h2>
       {info.experiences.map((experience, i) => (
-        <Box key={experience.id} sx={{ mb: 4 }}>
+        <Box key={experience.id} sx={{ mb: 4 }} className="experience-details">
           <h3>Experience section {i + 1}</h3>
           <ExperienceDataInput
             experienceDetails={experience}
@@ -110,33 +113,34 @@ const Form = ({
       </Button>
 
       <h2>Technical Skills</h2>
-      <Input
-        id="programmingLanguages"
-        name="programmingLanguages"
-        value={info.programmingLanguages}
-        onInputChange={onInputChange}
-      >
-        Programming Languages
-      </Input>
-      <br />
-      <Input
-        id="frameworks"
-        name="frameWorks"
-        value={info.frameWorks}
-        onInputChange={onInputChange}
-      >
-        Frameworks
-      </Input>
-      <br />
-      <Input
-        id="developerTools"
-        name="developerTools"
-        value={info.developerTools}
-        onInputChange={onInputChange}
-      >
-        Developer Tools
-      </Input>
-      <br />
+      <Box className="technical-skills-info">
+        <Input
+          id="programmingLanguages"
+          name="programmingLanguages"
+          value={info.programmingLanguages}
+          onInputChange={onInputChange}
+        >
+          Programming Languages
+        </Input>
+        <br />
+        <Input
+          id="frameworks"
+          name="frameWorks"
+          value={info.frameWorks}
+          onInputChange={onInputChange}
+        >
+          Frameworks
+        </Input>
+        <br />
+        <Input
+          id="developerTools"
+          name="developerTools"
+          value={info.developerTools}
+          onInputChange={onInputChange}
+        >
+          Developer Tools
+        </Input>
+      </Box>
     </div>
   );
 };
